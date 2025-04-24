356de9e359da023307ef31f4cdca6c0ae84f456a
Adarsh Shaw <adarshwshaw@gmail.com>
2025-04-25 00:52:24 +0530

    added language support for hindi

--- transcribe_service/app.py
+++ transcribe_service/app.py
@@ -1,9 +1,20 @@
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 import gradio as gr
+import torch
 
-def transcribe(ifile):
-    transcriber  = pipeline("automatic-speech-recognition", model="openai/whisper-tiny")
+langs = ["English", "Hindi"]
+
+device= 0 if torch.cuda.is_available() else -1
+print(device)
+
+def transcribe(lang, ifile):
+    transcriber=None
+    if lang == langs[0]:
+        transcriber  = pipeline("automatic-speech-recognition", model="openai/whisper-base",chunk_length_s=30,device=device,generate_kwargs = {"language":"en","task": "translate"})
+    else:
+        transcriber  = pipeline("automatic-speech-recognition", model="vasista22/whisper-hindi-small",chunk_length_s=30,device=device)
+        transcriber.model.config.forced_decoder_ids = transcriber.tokenizer.get_decoder_prompt_ids(language="hi", task="transcribe")
 
     # Path to your MP3 audio file
 
@@ -12,11 +23,10 @@ def transcribe(ifile):
     return transcription["text"] 
 
 
-audio_file = "sample.mp3"  # Replace with the actual path to your MP3 file
 
 demo = gr.Interface(
     fn=transcribe,
-    inputs=[gr.Audio(sources=['upload'],type='filepath',label='input',show_label=True)],
+    inputs=[gr.Dropdown(langs, label="Language", info="Give the language of the audio"),gr.Audio(sources=['upload'],type='filepath',label='input',show_label=True)],
     outputs=["text"],
     api_name="transcribe"
 )
